facc201be93dc234a8f3455fef6a547ec9aa8ff6
BillChien <61368521+bill37211207@users.noreply.github.com>
2024-09-18 14:54:03 +0800

    first commit2

---
+++ src/HelloWorld.js
@@ -0,0 +1,14 @@
+// src/HelloWorld.js
+
+import React from 'react';
+
+function HelloWorld() {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <p>This is a basic React.js application.</p>
+        </div>
+    );
+}
+
+export default HelloWorld;
